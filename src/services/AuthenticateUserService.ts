8844632f8b5d0a98ac68ae01e801f0e7df3da07d
RodrigoSaantos <contato.rodrigosaantos@gmail.com>
2021-10-25 21:49:58 +0000

    AccessToken github

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,10 +1,14 @@
 import axios from "axios";
 
+interface IAccessTokenResponse {
+  access_token: string;
+}
+
 class AuthenticateUserService {
   async execute(code: string) {
     const url = "https://github.com/login/oauth/access_token";
 
-    const response = await axios.post(url, null, { 
+    const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, { 
       params: {
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -14,6 +18,12 @@ class AuthenticateUserService {
         "Accept": "application/json",
       }
     });
+
+    const response = await axios.get("https://api.github.com/user", {
+      headers: {
+        authorization: `Bearer ${accessTokenResponse.access_token}`
+      }
+    })
 
     return response.data;
   }
