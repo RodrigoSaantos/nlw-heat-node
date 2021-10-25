e983a4c5e5ffde1cfaa173eb4c28de9158c4e397
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-10-24 21:31:32 -0300

    Add prisma and routes, signin and callback

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
+import "dotenv/config";
 import express from 'express';
 
 const app = express();
+
+app.get('/github', (request, response) => {
+  response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+});
+
+app.get("/signin/callback", (request, response) => {
+  const { code } = request.query;
+
+  return response.json(code);
+});
 
 app.listen(3333, () => {
   console.log(`✨ Server is running on port 3333`);
